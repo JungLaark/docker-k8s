6d30f10854628a091072e46a36ba06fe382a4434
JungLaark <chenone839@gmail.com>
2022-05-14 17:40:56 +0900

    first day

---
+++ docker/main.go
@@ -0,0 +1,21 @@
+//8080 요청 대기. /로 요청이 오면 응답
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("received request")
+		fmt.Fprintf(w, "Hello Docker222!!")
+	})
+
+	log.Println("start server")
+	server := &http.Server{Addr: ":8080"}
+	if err := server.ListenAndServe(); err != nil {
+		log.Println(err)
+	}
+}
